10aef15d98bf7825170fa8ec2f4d152597118fe1
xxs <bushiqisuo@126.com>
2022-09-14 21:18:16 +0800

    描述:登录，注册页面 提交人:xxs

--- app/src/main/java/com/example/mytik/MainActivity.java
+++ app/src/main/java/com/example/mytik/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.mytik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.mytik.activity.LoginActivity;
+import com.example.mytik.activity.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginBtn;
+
+    private Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerBtn = findViewById(R.id.registerBtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
