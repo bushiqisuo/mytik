c3aa7543fb95e6e309508f1841a2cb45f1e7d822
xxs <bushiqisuo@126.com>
2022-09-20 21:02:12 +0800

    描述:添加okhttp依赖及联网权限 提交人:xxs

--- app/src/main/java/com/example/mytik/activity/LoginActivity.java
+++ app/src/main/java/com/example/mytik/activity/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.mytik.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,8 +13,24 @@ import android.widget.Toast;
 import com.example.mytik.R;
 import com.example.mytik.util.TextUtil;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class LoginActivity extends BaseActivity {
     private static final String TAG = "LoginActivity";
+
+    private static final String URL_LOGIN = "http://www.baidu.com";
 
     private Button bigLogin;
 
@@ -25,20 +43,61 @@ public class LoginActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        accountInLogin = findViewById(R.id.accountInLogin);
-        pwdInLogin = findViewById(R.id.pwdInLogin);
-        bigLogin = findViewById(R.id.bigLogin);
+        initView();
+        initEvent();
+    }
+
+    private void initEvent() {
+        String account = accountInLogin.getText().toString();
+        String pwd = pwdInLogin.getText().toString();
         bigLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!TextUtil.isEmpty(pwdInLogin.getText().toString()) &&
-                !TextUtil.isEmpty(accountInLogin.getText().toString())) {
+                if (TextUtil.isEmpty(pwdInLogin.getText().toString()) ||
+                        TextUtil.isEmpty(accountInLogin.getText().toString())) {
+                    showToast("请输入账号密码");
+                } else {
                     showToast("账号:" + accountInLogin.getText().toString() +
                             "密码:" + pwdInLogin.getText().toString());
-                } else {
-                    showToast("请输入账号密码");
+                    Log.d(TAG, "开始请求登录接口");
+                    //1.client
+                    OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                            .build();
+                    //2.request
+                    //请求参数
+                    Map map = new HashMap();
+                    map.put("mobile", account);
+                    map.put("password", pwd);
+                    JSONObject jsonObject = new JSONObject(map);
+                    String toString = jsonObject.toString();
+                    RequestBody requestBody = RequestBody.create(MediaType.parse("application/json;charset=utf-8"), toString);
+                    Request request = new Request.Builder()
+                            .url(URL_LOGIN)
+                            .addHeader("contentType", "application/json;charset=utf-8")
+                            .post(requestBody)
+                            .build();
+                    //3.call
+                    Call call = okHttpClient.newCall(request);
+                    //4.post
+                    call.enqueue(new Callback() {
+                        @Override
+                        public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                            Log.e(TAG, "onFailure: " + e );
+                        }
+
+                        @Override
+                        public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                            Log.e(TAG, "onResponse: " + response.body().toString());
+                        }
+                    });
                 }
             }
         });
+    }
+
+    private void initView() {
+        accountInLogin = findViewById(R.id.accountInLogin);
+        pwdInLogin = findViewById(R.id.pwdInLogin);
+        bigLogin = findViewById(R.id.bigLogin);
     }
 }
--- app/src/main/java/com/example/mytik/activity/RegisterActivity.java
+++ app/src/main/java/com/example/mytik/activity/RegisterActivity.java
@@ -1,15 +1,28 @@
 package com.example.mytik.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+
 import com.example.mytik.R;
 import com.example.mytik.util.TextUtil;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class RegisterActivity extends BaseActivity {
     private static final String TAG = "RegisterActivity";
+
+    private static final String URL_REGISTER = "http://www.baidu.com";
 
     private Button bigRegister;
 
@@ -29,12 +42,17 @@ public class RegisterActivity extends BaseActivity {
         bigRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!TextUtil.isEmpty(accountInRegister.getText().toString()) &&
-                        !TextUtil.isEmpty(pwdInRegister.getText().toString())) {
-                    showToast("账号:" + accountInRegister.getText().toString() +
-                            "密码:" + pwdInRegister.getText().toString());
-                } else {
+                if (TextUtil.isEmpty(accountInRegister.getText().toString()) ||
+                        TextUtil.isEmpty(pwdInRegister.getText().toString())) {
                     showToast("请输入账号密码");
+                } else {
+                    //网络请求
+                    Log.d(TAG, "onClick: 开始请求注册接口");
+                    //1.client
+                    OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                            .build();
+                    //2.request
+
                 }
             }
         });
